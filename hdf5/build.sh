cf6db015d6d11fefa057b3698a8ed6b3551e1ccd
Mikael Mortensen <mikaem@math.uio.no>
2016-03-24 22:58:18 +0100

    Making Petsc compile more and getting dolfin to work with parmetis and hdf5

--- hdf5/build.sh
+++ hdf5/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # ln -s $PREFIX/bin/gfortran $PREFIX/bin/f95
-
-./configure --prefix=$PREFIX --enable-linux-lfs --with-zlib=$PREFIX --with-ssl --enable-parallel --enable-shared
+export CC=mpicc
+./configure --prefix=$PREFIX --enable-linux-lfs --with-default-api-version=v18 --enable-unsupported --enable-production=yes --with-pthread --with-zlib=$PREFIX --with-ssl --enable-parallel --enable-shared
 make -j${CPU_COUNT}
 make install
 
-rm -rf $PREFIX/share/hdf5_examples
+#rm -rf $PREFIX/share/hdf5_examples
