5e3ee16b6dec3f159b96b16455f9f31628e4556d
= <=>
2016-08-29 10:54:18 +0200

    Works on ubuntu 16.04

--- build_fenics.sh
+++ build_fenics.sh
@@ -1,21 +1,28 @@
 # Using host gcc
+# Make sure gfortran and cmake are installed on host.
+# sudo apt install gfortran cmake
+
 export CONDA_BUILD_DIR=/home/mikael/anaconda2/conda-bld/linux-64
-export FENICS_VERSION=2016.1
+export FENICS_VERSION=2016.2.dev
 export FENICS_GIT_TAG=2016.1.0
-export CONDA_BUILD_NUMBER=5
+export CONDA_BUILD_NUMBER=7
 export CONDA_BUILD_LABEL=dev
 
 conda config --add channels mikaem/label/${CONDA_BUILD_LABEL}
 
 # Dependency for this branch only
-# cd ../conda-recipes; conda build vtk; cd ../fenics-recipes
-# anaconda upload --force /home/mikael/anaconda2/conda-bld/linux-64/vtk-5.10.1-${CONDA_BUILD_NUMBER}.tar.bz2
+## Need to install boost myself because of incompatible icu (see https://github.com/ContinuumIO/anaconda-issues/issues/700)
+## cd ../conda-recipes
+## conda build vtk
+## anaconda upload --force ${CONDA_BUILD_DIR}/vtk-5.10.1-${CONDA_BUILD_NUMBER}.tar.bz2
+## conda build boost
+## anaconda upload --force ${CONDA_BUILD_DIR}/boost-1.60.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2
 
 # Other dependencies
-# conda build hdf5
-# anaconda upload --force ${CONDA_BUILD_DIR}/hdf5-1.8.14-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-# conda build h5py
-# anaconda upload --force ${CONDA_BUILD_DIR}/h5py-2.6.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build hdf5-parallel
+# anaconda upload --force ${CONDA_BUILD_DIR}/hdf5-parallel-1.8.14-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build h5py-parallel
+# anaconda upload --force ${CONDA_BUILD_DIR}/h5py-parallel-2.6.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # # conda build hwloc
 # # anaconda upload --force ${CONDA_BUILD_DIR}/hwloc-1.11.2-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build eigen3
@@ -27,21 +34,21 @@ conda config --add channels mikaem/label/${CONDA_BUILD_LABEL}
 # conda build slepc
 # anaconda upload --force ${CONDA_BUILD_DIR}/slepc-3.6.2-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 
-conda build instant
-anaconda upload --force ${CONDA_BUILD_DIR}/instant-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-conda build dijitso
-anaconda upload --force ${CONDA_BUILD_DIR}/dijitso-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-conda build ufl
-anaconda upload --force ${CONDA_BUILD_DIR}/ufl-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-conda build fiat
-anaconda upload --force ${CONDA_BUILD_DIR}/fiat-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build instant
+# anaconda upload --force ${CONDA_BUILD_DIR}/instant-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build dijitso
+# anaconda upload --force ${CONDA_BUILD_DIR}/dijitso-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build ufl
+# anaconda upload --force ${CONDA_BUILD_DIR}/ufl-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build fiat
+# anaconda upload --force ${CONDA_BUILD_DIR}/fiat-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 conda build ffc
 anaconda upload --force ${CONDA_BUILD_DIR}/ffc-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-conda build dolfin
-anaconda upload --force ${CONDA_BUILD_DIR}/dolfin-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build dolfin
+# anaconda upload --force ${CONDA_BUILD_DIR}/dolfin-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 conda build dolfin-vtk
 anaconda upload --force ${CONDA_BUILD_DIR}/dolfin-${FENICS_VERSION}-py27_vtk_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-conda build fenics
-anaconda upload --force ${CONDA_BUILD_DIR}/fenics-${FENICS_VERSION}-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build fenics
+# anaconda upload --force ${CONDA_BUILD_DIR}/fenics-${FENICS_VERSION}-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 conda build fenics-vtk
 anaconda upload --force ${CONDA_BUILD_DIR}/fenics-${FENICS_VERSION}-vtk_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
--- dolfin/build.sh
+++ dolfin/build.sh
@@ -12,11 +12,12 @@ export HDF5_DIR=$PREFIX
 export PETSC_DIR=$PREFIX
 export BLAS_DIR=$PREFIX/lib
 export LAPACK_DIR=$PREFIX/lib
+export BOOST_ROOT=$PREFIX
 # To make dolfin pick up anaconda swig:
 ln -s ${PREFIX}/bin/swig ${PREFIX}/bin/swig3.0
 
 cmake .. \
-  -DDOLFIN_ENABLE_VTK:BOOL=FALSE \
+  -DDOLFIN_ENABLE_VTK:BOOL=TRUE \
   -DDOLFIN_ENABLE_QT:BOOL=FALSE \
   -DDOLFIN_ENABLE_SCOTCH:BOOL=FALSE \
   -DDOLFIN_ENABLE_PASTIX:BOOL=FALSE \
@@ -32,9 +33,11 @@ cmake .. \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INCLUDE_PATH=$INCLUDE_PATH \
   -DCMAKE_LIBRARY_PATH=$LIBRARY_PATH \
-  -DBoost_INCLUDE_DIR=$INCLUDE_PATH \
-  -DBoost_LIBRARY_DIRS=$LIBRARY_PATH \
-  -DBoost_FILESYSTEM_LIBRARY=$LIBRARY_PATH/libboost_filesystem.so \
+  -DBoost_NO_BOOST_CMAKE=TRUE \
+  -DBOOST_ROOT=$LIBRARY_PATH \
+  -DBoost_INCLUDE_DIR=$PREFIX/include \
+  -DBoost_LIBRARY_DIRS=$PREFIX \
+  -DBoost_FILESYSTEM_LIBRARY=$PREFIX/lib/libboost_filesystem.so \
   -DMPI_C_LIBRARIES=$LIBRARY_PATH/libmpich.so \
   -DMPI_C_INCLUDE_PATH=$INCLUDE_PATH \
   -DMPI_CXX_LIBRARIES=$LIBRARY_PATH/libmpichcxx.so \
@@ -44,3 +47,7 @@ cmake .. \
 
 make -j${CPU_COUNT}
 make install
+
+#   -DBoost_INCLUDE_DIR=/usr/include \
+#   -DBoost_LIBRARY_DIRS=/usr/lib/x86_64-linux-gnu \
+#   -DBoost_FILESYSTEM_LIBRARY=/usr/lib/x86_64-linux-gnu/libboost_filesystem.so \
