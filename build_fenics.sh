71ddc6c991ad78c3b3ac992cab225aa314cbe952
= <=>
2016-09-01 19:03:57 +0200

    Minor

--- build_fenics.sh
+++ build_fenics.sh
@@ -5,34 +5,34 @@
 export CONDA_BUILD_DIR=/home/mikael/anaconda2/conda-bld/linux-64
 export FENICS_VERSION=2016.2.dev
 export FENICS_GIT_TAG=2016.1.0
-export CONDA_BUILD_NUMBER=7
+export CONDA_BUILD_NUMBER=11
 export CONDA_BUILD_LABEL=dev
 
 conda config --add channels mikaem/label/${CONDA_BUILD_LABEL}
 
-# Dependency for this branch only
-## Need to install boost myself because of incompatible icu (see https://github.com/ContinuumIO/anaconda-issues/issues/700)
-## cd ../conda-recipes
-## conda build vtk
-## anaconda upload --force ${CONDA_BUILD_DIR}/vtk-5.10.1-${CONDA_BUILD_NUMBER}.tar.bz2
-## conda build boost
-## anaconda upload --force ${CONDA_BUILD_DIR}/boost-1.60.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2
-
-# Other dependencies
+# # Dependency for this branch only
+# ## Need to install boost myself because of incompatible icu (see https://github.com/ContinuumIO/anaconda-issues/issues/700)
+# cd ../conda-recipes
+# ## conda build vtk
+# ## anaconda upload --force ${CONDA_BUILD_DIR}/vtk-5.10.1-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build boost
+# anaconda upload --force ${CONDA_BUILD_DIR}/boost-1.60.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# # Other dependencies
+# cd ../fenics-recipes
 # conda build hdf5-parallel
 # anaconda upload --force ${CONDA_BUILD_DIR}/hdf5-parallel-1.8.14-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build h5py-parallel
 # anaconda upload --force ${CONDA_BUILD_DIR}/h5py-parallel-2.6.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-# # conda build hwloc
-# # anaconda upload --force ${CONDA_BUILD_DIR}/hwloc-1.11.2-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# # # conda build hwloc
+# # # anaconda upload --force ${CONDA_BUILD_DIR}/hwloc-1.11.2-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build eigen3
 # anaconda upload --force ${CONDA_BUILD_DIR}/eigen3-3.2.1-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build petsc
-# anaconda upload --force ${CONDA_BUILD_DIR}/petsc-3.6.2-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# anaconda upload --force ${CONDA_BUILD_DIR}/petsc-3.7.3-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build petsc4py
-# anaconda upload --force ${CONDA_BUILD_DIR}/petsc4py-3.6.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# anaconda upload --force ${CONDA_BUILD_DIR}/petsc4py-3.7.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build slepc
-# anaconda upload --force ${CONDA_BUILD_DIR}/slepc-3.6.2-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# anaconda upload --force ${CONDA_BUILD_DIR}/slepc-3.7.2-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 
 # conda build instant
 # anaconda upload --force ${CONDA_BUILD_DIR}/instant-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
@@ -42,12 +42,12 @@ conda config --add channels mikaem/label/${CONDA_BUILD_LABEL}
 # anaconda upload --force ${CONDA_BUILD_DIR}/ufl-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build fiat
 # anaconda upload --force ${CONDA_BUILD_DIR}/fiat-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-conda build ffc
-anaconda upload --force ${CONDA_BUILD_DIR}/ffc-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build ffc
+# anaconda upload --force ${CONDA_BUILD_DIR}/ffc-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build dolfin
 # anaconda upload --force ${CONDA_BUILD_DIR}/dolfin-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
-conda build dolfin-vtk
-anaconda upload --force ${CONDA_BUILD_DIR}/dolfin-${FENICS_VERSION}-py27_vtk_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+# conda build dolfin-vtk
+# anaconda upload --force ${CONDA_BUILD_DIR}/dolfin-${FENICS_VERSION}-py27_vtk_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 # conda build fenics
 # anaconda upload --force ${CONDA_BUILD_DIR}/fenics-${FENICS_VERSION}-${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 conda build fenics-vtk
